b97cf160e38ed0d2b2a447e1478a128b64eac2b0
Zverev <kruchkovevg21@gmail.com>
2019-12-21 04:56:58 +0300

    REDIRECT REMOVED

--- DZ-1/routes/form/index.js
+++ DZ-1/routes/form/index.js
@@ -1,24 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const validate = require("../../helpers/validateForm");
+const validate = require('../../helpers/validateForm');
 
-router.get("/", (req, res) => {
-  res.render("form", {
-    title: "Login",
+router.get('/', (req, res) => {
+  res.render('form', {
+    title: 'Login',
     fields: {
-      username: "",
-      password: ""
+      username: '',
+      password: ''
     }
   });
 });
-router.post("/", (req, res) => {
-  res.redirect(301, "/form");
-  res.render("form", {
-    title: "Login",
+router.post('/', (req, res) => {
+  // res.redirect(301, "/form");
+  res.render('form', {
+    title: 'Login',
     validation: validate(req.body),
     fields: req.body
   });
-  // res.sendStatus(302);
 });
 
 module.exports = router;
